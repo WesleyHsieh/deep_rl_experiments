813b9b18118e9b278d3b07acb13bcd5b60aff159
Wesley Hsieh <wesley.y.hsieh@gmail.com>
2016-10-21 20:42:26 -0700

    Added plotting code

--- experiment_runner.py
+++ experiment_runner.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 from hyperparameter_tuning import HyperparameterTuner
 
 from rllab.algos.trpo import TRPO
@@ -16,13 +17,16 @@ class ExperimentRunner:
     def __init__(self, hyperparam_dict, env_name):
         self.hyperparam_dict = hyperparam_dict
         self.env_name = env_name
+        self.exp_fields = ['AverageDiscountedReturn', 'AverageReturn']
 
-    def run_experiments(self):
+    def run_experiments(self, plot=True):
         hyperparam_lists = HyperparameterTuner(self.hyperparam_dict, 'grid')\
             .generate_hyperparam_lists()
 
         exp_results = [self.run_single_experiment(exp_num, hyperparam_lists[exp_num])
                        for exp_num in range(len(hyperparam_lists))]
+        if plot:
+            self.plot_results(exp_results)
         return exp_results
 
     def run_single_experiment(self, exp_num, hyperparams):
@@ -56,14 +60,27 @@ class ExperimentRunner:
             exp_name=exp_name
         )
         exp_filepath = os.path.join(EXPDIR, exp_name, 'progress.csv')
-        result_dict = self.parse_results(exp_filepath)
+        result_dict = self.parse_experiment_results(exp_filepath)
         return hyperparams, result_dict
 
-    def parse_results(self, exp_filepath):
-        fields = ['AverageDiscountedReturn', 'AverageReturn']
+    def parse_experiment_results(self, exp_filepath):
         with open(exp_filepath) as csvfile:
             reader = csv.DictReader(csvfile)
             row = [r for r in reader][-1]
-            result_dict = {field: row[field] for field in fields}
+            result_dict = {field: float(row[field]) for field in self.exp_fields}
             return result_dict
+
+    def plot_results(self, exp_results):
+        for field in self.exp_fields:
+            best_result = max(exp_results, key=lambda x:x[1][field])
+            field_entries = [result[1][field] for result in exp_results]
+            plt.figure()
+            plt.hist(field_entries)
+            plt.title('Distribution of {}, Environment {}'.format(field, self.env_name))
+            plt.savefig('distribution_{}_env_{}'.format(field, self.env_name))
+            print('Best Result, Hyperparameters for Field {}: {}, {}'.format(field, best_result[1], best_result[0]))
+
+
+
+
 
